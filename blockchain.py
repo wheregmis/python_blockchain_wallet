02f12d6cc00a7de9b12413979b20a01d33aad8c1
Sabin Regmi <xawbeenregmi@gmail.com>
2021-11-01 22:40:49 +0545

    Balance Calculation Added

--- blockchain.py
+++ blockchain.py
@@ -7,12 +7,27 @@ genesis_block = {'previous_hash': '',
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Sabin'
+participants = {'Sabin'}
 
 
 def hash_block(block):
     return '-'.join([str(block[key]) for key in block])
 
-# this function returns the last blockchain value
+
+def get_balance(participant):
+    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant]
+                 for block in blockchain]
+    amount_sent = 0
+    for tx in tx_sender:
+        if len(tx) > 0:
+            amount_sent += tx[0]
+    tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant]
+                    for block in blockchain]
+    amount_received = 0
+    for tx in tx_recipient:
+        if len(tx) > 0:
+            amount_received += tx[0]
+    return amount_received - amount_sent
 
 
 def get_last_blockchain_value():
@@ -37,17 +52,19 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     """
     transaction = {'sender': sender, 'recipient': recipient, 'amount': amount}
     open_transactions.append(transaction)
+    participants.add(sender)
+    participants.add(recipient)
 
 
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
-    print(hashed_block)
     block = {'previous_hash': hashed_block,
              'index': len(blockchain),
              'transactions': open_transactions
              }
     blockchain.append(block)
+    return True
 
 
 def get_transaction_value():
@@ -90,6 +107,7 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output the participants')
     print('h: Manipulate the chain')
     print('q: Quit the program')
     user_choice = get_user_choice()
@@ -100,10 +118,14 @@ while waiting_for_input:
         print(open_transactions)
     elif user_choice == '2':
         # Mine a block
-        mine_block()
+        if mine_block():
+            open_transactions = []
     elif user_choice == '3':
         # Output the blockchain list to the console
         print_blockchain_elements()
+    elif user_choice == '4':
+        # Output the participants list to the console
+        print(participants)
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {'previous_hash': '',
@@ -118,6 +140,7 @@ while waiting_for_input:
         print_blockchain_elements()
         print('Invalid Blockchain')
         break
+    print(get_balance('Sabin'))
 else:
     print('User left!')
 
